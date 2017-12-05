f188e2826ed10292a7e6361e63a5207c62835987
Unknown <zacharyd1@live.com>
2017-12-05 13:42:22 -0500

    Wrote most of the time class

--- Homework 12/Homework 12/time.cpp
+++ Homework 12/Homework 12/time.cpp
@@ -1,4 +1,137 @@
-
+#include <iostream>
 
 #include "time.h"
 
+time::time()
+{
+	sec = 0;
+	min = 0;
+}
+
+time::time(int seconds)
+{
+	sec = seconds;
+	min = 0;
+
+	roundValues();
+}
+
+time::time(int seconds, int minutes)
+{
+	sec = seconds;
+	min = minutes;
+	roundValues();
+}
+
+void time::roundValues()
+{
+	if(sec < 0)	//If we have a negative value for seconds, convert minutes to seconds and add it
+	{
+		sec += min * 60;
+		min = 0;
+
+		if(sec < 0)	//If the net time is negative, we know that the value as a whole is negative.  Therefore, set the negative flag, and make sec & min positive
+		{
+			sec *= -1;
+			neg = true;
+		}
+
+	}
+
+	if(sec >= 60)		//Convert any value of sec into as many minutes as possible
+	{
+		min += sec / 60;
+		sec = sec % 60;
+	}
+	
+}
+
+time time::operator+(time & other)
+{
+	return time(getTotal() + other.getTotal());
+}
+
+void time::operator+=(time & other)
+{
+	*this = getTotal() + other.getTotal();
+}
+
+time time::operator-(time & other)
+{
+	return time(getTotal() - other.getTotal());
+}
+
+void time::operator-=(time & other)
+{
+	*this = getTotal() - other.getTotal();
+}
+
+time time::operator*(time & other)
+{
+	time(getTotal() * other.getTotal());
+}
+
+time time::operator*(int & other)
+{
+	time(getTotal() * other);
+}
+
+void time::operator*=(time & other)
+{
+	*this = *this * other;
+}
+
+void time::operator*=(int & other)
+{
+	*this = *this * other;
+}
+
+time time::operator/(time & other)
+{
+	return time(getTotal() / other.getTotal());
+}
+
+time time::operator/(int & other)
+{
+	return time(getTotal() / other);
+}
+
+void time::operator/=(time & other)
+{
+	*this = *this / other;
+}
+
+void time::operator/=(int & other)
+{
+	*this = *this / other;
+}
+
+bool time::operator==(time & other)
+{
+	return getTotal() == other.getTotal();
+}
+
+bool time::operator<(time & other)
+{
+	return getTotal() < other.getTotal();
+}
+
+bool time::operator<=(time & other)
+{
+	return getTotal() <= other.getTotal();
+}
+
+bool time::operator>(time & other)
+{
+	return getTotal() > other.getTotal();
+}
+
+bool time::operator>=(time & other)
+{
+	return getTotal() >= other.getTotal();
+}
+
+void const time::print()
+{
+	std::cout << "min: " << min << " sec: " << sec << std::endl;
+}
--- Homework 12/Homework 12/time.h
+++ Homework 12/Homework 12/time.h
@@ -1,6 +1,43 @@
+#pragma once
+
 class time
 {
+public:
+	time();
 	time(int seconds);
-	time(int minutes, int seconds);
+	time(int seconds, int minutes);
 
+private:
+	int min = 0;
+	int sec = 0;
+	bool neg = false;	//If the value is negated
+
+public:
+	void roundValues();	//Validates that sec < 60, converts to minutes as needed
+
+	//NOTE: getMin() and getSec() do NOT include the negative, if the value is negative
+	int const getMin() { return min; }
+	int const getSec() { return sec; }
+	int const getTotal() { return (neg == true? -1 : 1) * (min * 60 + sec); }
+
+	time operator+(time & other);
+	void operator+=(time & other);
+	time operator-(time & other);
+	void operator-=(time & other);
+
+	time operator*(time & other);
+	time operator*(int & other);
+	void operator*=(time & other);
+	void operator*=(int & other);
+	time operator/(time & other);
+	time operator/(int & other);
+	void operator/=(time & other);
+	void operator/=(int & other);
+
+	bool operator<(time & other);
+	bool operator<=(time & other);
+	bool operator>(time & other);
+	bool operator>=(time & other);
+
+	void const print();
 };
